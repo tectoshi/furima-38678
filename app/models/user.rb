46f34c9719f325cf312b3c153eb2f1f5559c10d3
tectoshi <116927616+tectoshi@users.noreply.github.com>
2022-11-18 20:41:37 +0900

    新規登録機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,17 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable         
+  
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  #format:{ with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Last name is invalid. Input full-width characters' }
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  #format: {with:  /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/,message: "全角カタカナのみで入力して下さい"}
+  
+  validates :first_name_kana, presence: true
+  validates :date_of_birth, presence: true
+
+
 end
