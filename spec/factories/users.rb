fb286f25d2b0ec2b55dd0d87d9fe2fcb18f61219
tectoshi <116927616+tectoshi@users.noreply.github.com>
2022-11-19 13:48:21 +0900

    FactoryBot改修

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,14 +3,14 @@ FactoryBot.define do
     transient do
       person { Gimei.name }
     end
-    nickname {Faker::Name.name}
-    email {Faker::Internet.free_email}
-    password { "a1" + Faker::Internet.password(min_length: 4)}
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              { "a1" + Faker::Internet.password(min_length: 4)}
     password_confirmation {password}
-    last_name {person.last.kanji}
-    first_name {person.first.kanji}
-    last_name_kana {person.last.katakana}
-    first_name_kana {person.first.katakana}
-    date_of_birth {Faker::Date.birthday(min_age: 18, max_age: 65)}
+    last_name             {person.last.kanji}
+    first_name            {person.first.kanji}
+    last_name_kana        {person.last.katakana}
+    first_name_kana       {person.first.katakana}
+    date_of_birth         {Faker::Date.birthday(min_age: 18, max_age: 65)}
   end
 end
