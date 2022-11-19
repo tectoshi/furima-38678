5af56820c9ec58bf4ed07fb82d17ce97421c1bda
tectoshi <116927616+tectoshi@users.noreply.github.com>
2022-11-19 13:43:39 +0900

    FactoryBot日本語対応

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,12 +5,12 @@ FactoryBot.define do
     end
     nickname {Faker::Name.name}
     email {Faker::Internet.free_email}
-    password {Faker::Internet.password(min_length: 6)}
+    password { "a1" + Faker::Internet.password(min_length: 4)}
     password_confirmation {password}
-    last_name {Faker::Name.last_name.katakana}
-    first_name {Faker::Name.first_name.katakana}
-    last_name_kana {Faker::Name.last_name.katakana}
-    first_name_kana {Faker::Name.first_name.katakana}
+    last_name {person.last.kanji}
+    first_name {person.first.kanji}
+    last_name_kana {person.last.katakana}
+    first_name_kana {person.first.katakana}
     date_of_birth {Faker::Date.birthday(min_age: 18, max_age: 65)}
   end
 end
