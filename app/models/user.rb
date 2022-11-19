dcc5740fbff9b122249835bbd1eb389e107b0808
tectoshi <116927616+tectoshi@users.noreply.github.com>
2022-11-19 11:21:12 +0900

    password英数字混合バリデーション実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable         
   
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  #format:{ with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Last name is invalid. Input full-width characters' }
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  #format: {with:  /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/,message: "全角カタカナのみで入力して下さい"}
-  
-  validates :first_name_kana, presence: true
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :last_name_kana, presence: true, format: { with:  /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/}
+  validates :first_name_kana, presence: true, format: { with:  /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/}
   validates :date_of_birth, presence: true
 
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
 
 end
