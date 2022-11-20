c73bff7b0e022c2c8690e3f4a72aaba562668e95
tectoshi <116927616+tectoshi@users.noreply.github.com>
2022-11-21 02:44:39 +0900

    未ログイン時に出品ボタンを押すとログインページに遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,7 +24,7 @@ class ItemsController < ApplicationController
 
   def move_to_index
     unless user_signed_in?
-      redirect_to action: :index
+      redirect_to user_session_path
     end
   end
 end
