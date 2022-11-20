d66e9e7248456f3b883c3549faeb9c9b4b089f33
tectoshi <116927616+tectoshi@users.noreply.github.com>
2022-11-20 10:21:52 +0900

    itemモデルバリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,11 @@
 class Item < ApplicationRecord
   validates :name,                  presence: true
   validates :description,           presence: true
-  validates :category_id,           presence: true
-  validates :sales_status_id,       presence: true
-  validates :fee_status_id,         presence: true
-  validates :prefecture_id,         presence: true
-  validates :scheduled_delivery_id, presence: true
+  validates :category_id,           presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :sales_status_id,       presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :fee_status_id,         presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id,         presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :scheduled_delivery_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :price,                 presence: true
   
 end
