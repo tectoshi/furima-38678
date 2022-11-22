b9bf6bcf6985d6cf246091780fae4d35304bd2cd
tectoshi <116927616+tectoshi@users.noreply.github.com>
2022-11-22 15:36:27 +0900

    商品情報編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :move_to_index, only: :edit
   
   def index
     @items = Item.all.order(created_at: :desc)
@@ -18,6 +19,18 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+  def edit
+    @item = Item.find(params[:id])
+  end
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
+  end
+
 
  private
   def item_params
@@ -25,5 +38,10 @@ class ItemsController < ApplicationController
           .permit(:name, :description, :category_id, :sales_status_id, :fee_status_id, :prefecture_id, :scheduled_delivery_id, :price, :image)
           .merge(user_id: current_user.id)
   end
-
+  def move_to_index
+    if current_user.id != params[:user_id]
+      redirect_to root_path
+    end
+  end
+  
 end
