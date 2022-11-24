6ce229e76dd0ca86d778c3b454237f9c4132d087
tectoshi <116927616+tectoshi@users.noreply.github.com>
2022-11-25 04:24:29 +0900

    addressモデル作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,22 @@
 class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    @order = Order.new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @order = Order.new(order_params)
+    if @order.valid?
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+  def order_params
+    params.merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
