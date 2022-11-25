36309b544bda34d44ea079cba0cbc707be6c8b26
tectoshi <116927616+tectoshi@users.noreply.github.com>
2022-11-25 17:35:20 +0900

    購入機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,7 +7,9 @@ class OrdersController < ApplicationController
   def create
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
+    
     if @order_address.valid?
+      pay_item
       @order_address.save
       return redirect_to root_path
     else
@@ -19,6 +21,14 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:order_address)
           .permit(:postal_code, :prefecture_id, :city, :address, :phone_number, :building)
-          .merge(user_id: current_user.id, item_id: params[:item_id])
+          .merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+  def pay_item
+    Payjp.api_key =  ENV["PAYJP_SECRET_KEY"] 
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
   end
 end
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -7,10 +7,10 @@ const pay = () => {
     const formData = new FormData(formResult);
 
     const card = {
-      number: formData.get("order[number]"),
-      exp_month: formData.get("order[exp_month]"),
-      exp_year: `20${formData.get("order[exp_year]")}`,
-      cvc: formData.get("order[cvc]"),
+      number: formData.get("order_address[number]"),
+      exp_month: formData.get("order_address[exp_month]"),
+      exp_year: `20${formData.get("order_address[exp_year]")}`,
+      cvc: formData.get("order_address[cvc]"),
     };
 
     Payjp.createToken(card, (status, response) => {
@@ -24,7 +24,7 @@ const pay = () => {
       document.getElementById("card-exp-month").removeAttribute("name");
       document.getElementById("card-exp-year").removeAttribute("name");
       document.getElementById("card-cvc").removeAttribute("name");
-
+      
       document.getElementById("charge-form").submit();
     });
   });
