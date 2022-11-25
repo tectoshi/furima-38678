04d1ec148dfef36dc5581c0c5ceb055bd7549b4d
tectoshi <116927616+tectoshi@users.noreply.github.com>
2022-11-25 15:00:39 +0900

    orderモデルのバリデーション

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -2,14 +2,17 @@ class DonationAddress
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, 
                 :city, :addresses, :phone_number, :building
-
+ 
   with_options presence: true do
+    validates :user_id
+    validates :item_id
     validates :postal_code,   format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
     validates :city 
     validates :addresses
     validates :phone_number,  format: { with: /\A[0-9]{10,11}\z/, message: 'is invalid' }
   end
+  
   def save
     # 購入履歴を保存し、変数orderに代入する
     order = Order.create(user_id: user_id, :item_id item_id)
